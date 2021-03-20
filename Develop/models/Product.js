38be1055b1fe71219b1bffe06395db6ce539b410
Tori Sanford <tori.danielle.clark91@gmail.com>
2021-03-20 11:48:52 -0400

    finishing model and routes

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -20,26 +20,29 @@ Product.init(
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
     },
-    stock: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 10,
-    validate: {
-      isNumeric: true,
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
     },
   },
-  category_id: {
-  type: DataTypes.INTEGER,
-  references: {
-    model: "category",
-    key: "id",
-  },
-},
+  // go to sequelize
   {
     sequelize,
     timestamps: false,
